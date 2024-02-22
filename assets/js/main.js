25bedf57dc9716e313a2db55712c10f502bed618
Kishor S <kishorsbca@gmail.com>
2024-02-22 18:01:36 +0530

    getting api data using Xhr

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-// Write your javascript code here
+const blockQuoteEl = document.querySelector("blockquote");
+
+const xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    const res = JSON.parse(xhr.responseText);
+
+    blockQuoteEl.innerText = res.slip.advice;
+  }
+};
+
+xhr.open("GET", "https://api.adviceslip.com/advice", true);
+xhr.send();
