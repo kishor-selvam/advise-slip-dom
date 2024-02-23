4c40df57c51c2ca275fed37cba79211f77b54658
Kishor S <kishorsbca@gmail.com>
2024-02-23 11:00:35 +0530

    added new generate button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,42 @@
-const blockQuoteEl = document.querySelector("blockquote");
+const blockQuoteEl = document.querySelector("blockquote>p");
+const GenerateButtonEl = document.querySelector("#generate-btn");
 
-const xhr = new XMLHttpRequest();
+const API_URL = "https://api.adviceslip.com/advice";
 
-xhr.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200) {
-    const res = JSON.parse(xhr.responseText);
+GenerateButtonEl.style.display = "none";
 
-    blockQuoteEl.innerText = res.slip.advice;
+GenerateButtonEl.addEventListener("click", async () => {
+  const AdviceSlip = await adviceSlipApiCall();
+  blockQuoteEl.textContent = AdviceSlip.slip.advice;
+});
+
+// Using XHR
+
+// const xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//   if (this.readyState === 4 && this.status === 200) {
+//     const res = JSON.parse(xhr.responseText);
+
+//     blockQuoteEl.innerText = res.slip.advice;
+//   }
+// };
+
+// xhr.open("GET", API_URL, true);
+// xhr.send();
+
+// Using Await Async
+
+const adviceSlipApiCall = async () => {
+  try {
+    const res = await fetch(API_URL);
+    const jsonData = await res.json();
+    return jsonData;
+  } catch (err) {
+    console.log(err);
   }
 };
 
-xhr.open("GET", "https://api.adviceslip.com/advice", true);
-xhr.send();
+const AdviceSlip = await adviceSlipApiCall();
+blockQuoteEl.textContent = AdviceSlip.slip.advice;
+
+GenerateButtonEl.style.display = "inline-flex";
